16f23307569a961e90d40980b878a3b237cb9bf6
Hamizanoor <hamizanoor991@gmail.com>
2023-06-19 01:28:45 +0500

    emailpassword

---
+++ quiz.dart
@@ -0,0 +1,9 @@
+void main() {
+  String email = ("hamizanoor@gmail.com");
+  String password = ("2345");
+  if (email == "hamizanoor@gmail.com" && password == "2345") {
+    print("true");
+  } else {
+    print("false");
+  }
+}
